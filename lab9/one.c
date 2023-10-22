e035f77027a936ec5d02a367f2fc1323cf82ed12
sahil <404salad@gmail.com>
2023-10-22 10:24:24 +0530

    works!

--- lab9/one.c
+++ lab9/one.c
@@ -1,200 +1,92 @@
-#include <sys/types.h>
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
 #include <pthread.h>
+#include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/sem.h>
 #include <unistd.h>
 
-#define SEM_MUTEX_KEY "/tmp/sem-mutex-key"
-#define SEM_BUFFER_COUNT_KEY "/tmp/sem-buffer-count-key"
-#define SEM_SPOOL_SIGNAL_KEY "/tmp/sem-spool-signal-key"
-#define MAX_BUFFERS 10
+#define BUFFER_SIZE 5
+#define NUM_PRODUCERS 2
+#define NUM_CONSUMERS 3
 
-char buf[MAX_BUFFERS][100];
-int buffer_index;
-int buffer_print_index;
-int mutex_sem, buffer_count_sem, spool_signal_sem;
+int semid;
+int buffer[BUFFER_SIZE];
+int in = 0, out = 0;
 
-void *producer(void *arg);
-void *spooler(void *arg);
-
-int main() {
-    key_t s_key;
-    union semun {
-        int val;
-        struct semid_ds *buf;
-        ushort array[1];
-    } sem_attr;
-    pthread_t tid_producer[10], tid_spooler;
-    int i, r;
-
-    buffer_index = buffer_print_index = 0;
-
-    if ((s_key = ftok(SEM_MUTEX_KEY, 'a')) == -1) {
-        perror("ftok");
-        exit(1);
-    }
-    if ((mutex_sem = semget(s_key, 1, 0660 | IPC_CREAT)) == -1) {
-        perror("semget");
-        exit(1);
-    }
-    sem_attr.val = 1;
-    if (semctl(mutex_sem, 0, SETVAL, sem_attr) == -1) {
-        perror("semctl SETVAL");
-        exit(1);
-    }
-
-    if ((s_key = ftok(SEM_BUFFER_COUNT_KEY, 'a')) == -1) {
-        perror("ftok");
-        exit(1);
-    }
-    if ((buffer_count_sem = semget(s_key, 1, 0660 | IPC_CREAT)) == -1) {
-        perror("semget");
-        exit(1);
-    }
-    sem_attr.val = MAX_BUFFERS;
-    if (semctl(buffer_count_sem, 0, SETVAL, sem_attr) == -1) {
-        perror(" semctl SETVAL ");
-        exit(1);
-    }
-
-    if ((s_key = ftok(SEM_SPOOL_SIGNAL_KEY, 'a')) == -1) {
-        perror("ftok");
-        exit(1);
-    }
-    if ((spool_signal_sem = semget(s_key, 1, 0660 | IPC_CREAT)) == -1) {
-        perror("semget");
-        exit(1);
-    }
-    sem_attr.val = 0;
-    if (semctl(spool_signal_sem, 0, SETVAL, sem_attr) == -1) {
-        perror(" semctl SETVAL ");
-        exit(1);
-    }
-
-    if ((r = pthread_create(&tid_spooler, NULL, spooler, NULL)) != 0) {
-        fprintf(stderr, "Error = %d (%s)\n", r, strerror(r));
-        exit(1);
-    }
-
-    int thread_no[10];
-    for (i = 0; i < 10; i++) {
-        thread_no[i] = i;
-        if ((r = pthread_create(&tid_producer[i], NULL, producer, (void *)&thread_no[i])) != 0) {
-            fprintf(stderr, "Error = %d (%s)\n", r, strerror(r));
-            exit(1);
-        }
-    }
-
-    for (i = 0; i < 10; i++) {
-        if ((r = pthread_join(tid_producer[i], NULL)) == -1) {
-            fprintf(stderr, "Error = %d (%s)\n", r, strerror(r));
-            exit(1);
-        }
-    }
-
-    struct sembuf asem[1];
-    asem[0].sem_num = 0;
-    asem[0].sem_op = 0;
-    asem[0].sem_flg = 0;
-    if (semop(spool_signal_sem, asem, 1) == -1) {
-        perror("semop: spool_signal_sem");
-        exit(1);
-    }
-
-    if ((r = pthread_cancel(tid_spooler)) != 0) {
-        fprintf(stderr, "Error = %d (%s)\n", r, strerror(r));
-        exit(1);
-    }
-
-    if (semctl(mutex_sem, 0, IPC_RMID) == -1) {
-        perror("semctl IPC_RMID");
-        exit(1);
-    }
-    if (semctl(buffer_count_sem, 0, IPC_RMID) == -1) {
-        perror("semctl IPC_RMID");
-        exit(1);
-    }
-    if (semctl(spool_signal_sem, 0, IPC_RMID) == -1) {
-        perror("semctl IPC_RMID");
-        exit(1);
-    }
-
-    exit(0);
+void produce(int item) {
+    buffer[in] = item;
+    in = (in + 1) % BUFFER_SIZE;
 }
 
-void *producer(void *arg) {
-    int i;
-    int my_id = *((int *)arg);
-    struct sembuf asem[1];
-    int count = 0;
-
-    asem[0].sem_num = 0;
-    asem[0].sem_op = 0;
-    asem[0].sem_flg = 0;
-
-    for (i = 0; i < 10; i++) {
-        asem[0].sem_op = -1;
-        if (semop(buffer_count_sem, asem, 1) == -1) {
-            perror("semop: buffer_count_sem");
-            exit(1);
-        }
-
-        asem[0].sem_op = -1;
-        if (semop(mutex_sem, asem, 1) == -1) {
-            perror("semop: mutex_sem");
-            exit(1);
-        }
-
-        int j = buffer_index;
-        buffer_index++;
-        if (buffer_index == MAX_BUFFERS)
-            buffer_index = 0;
-
-        asem[0].sem_op = 1;
-        if (semop(mutex_sem, asem, 1) == -1) {
-            perror("semop: mutex_sem");
-            exit(1);
-        }
-
-        sprintf(buf[j], "Thread %d: %d\n", my_id, ++count);
-        asem[0].sem_op = 1;
-        if (semop(spool_signal_sem, asem, 1) == -1) {
-            perror("semop: spool_signal_sem");
-            exit(1);
-        }
-
-        sleep(1);
-    }
+int consume() {
+    int item = buffer[out];
+    out = (out + 1) % BUFFER_SIZE;
+    return item;
 }
 
-void *spooler(void *arg) {
-    struct sembuf asem[1];
-
-    asem[0].sem_num = 0;
-    asem[0].sem_op = 0;
-    asem[0].sem_flg = 0;
+void* producer(void* arg) {
+    int producer_id = *((int*)arg);
+    int item = 1;
 
     while (1) {
-        asem[0].sem_op = -1;
-        if (semop(spool_signal_sem, asem, 1) == -1) {
-            perror("semop: spool_signal_sem");
-            exit(1);
-        }
+        // Produce item
+        printf("Producer %d is producing item %d.\n", producer_id, item);
+        produce(item);
 
-        printf("%s", buf[buffer_print_index]);
+        // Sleep for a random time
+        sleep(1);
 
-        buffer_print_index++;
-        if (buffer_print_index == MAX_BUFFERS)
-            buffer_print_index = 0;
-
-        asem[0].sem_op = 1;
-        if (semop(buffer_count_sem, asem, 1) == -1) {
-            perror("semop: buffer_count_sem");
-            exit(1);
-        }
+        item++;
     }
 }
 
+void* consumer(void* arg) {
+    int consumer_id = *((int*)arg);
+
+    while (1) {
+        // Consume item
+        int item = consume();
+        printf("Consumer %d is consuming item %d.\n", consumer_id, item);
+
+        // Sleep for a random time
+        sleep(2);
+    }
+}
+
+int main() {
+    int producer_ids[NUM_PRODUCERS];
+    int consumer_ids[NUM_CONSUMERS];
+    pthread_t producers[NUM_PRODUCERS];
+    pthread_t consumers[NUM_CONSUMERS];
+
+    // Create a semaphore set with two semaphores (producer and consumer)
+    semid = semget(IPC_PRIVATE, 2, IPC_CREAT | 0666);
+    
+    // Initialize producer semaphore to buffer size (5)
+    semctl(semid, 0, SETVAL, BUFFER_SIZE);
+    // Initialize consumer semaphore to 0 (no items to consume initially)
+    semctl(semid, 1, SETVAL, 0);
+
+    for (int i = 0; i < NUM_PRODUCERS; i++) {
+        producer_ids[i] = i;
+        pthread_create(&producers[i], NULL, producer, &producer_ids[i]);
+    }
+
+    for (int i = 0; i < NUM_CONSUMERS; i++) {
+        consumer_ids[i] = i;
+        pthread_create(&consumers[i], NULL, consumer, &consumer_ids[i]);
+    }
+
+    for (int i = 0; i < NUM_PRODUCERS; i++) {
+        pthread_join(producers[i], NULL);
+    }
+
+    for (int i = 0; i < NUM_CONSUMERS; i++) {
+        pthread_join(consumers[i], NULL);
+    }
+
+    // Cleanup the semaphore set
+    semctl(semid, 0, IPC_RMID);
+
+    return 0;
+}
--- lab9/two.c
+++ lab9/two.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <pthread.h>
-#include <semaphore.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
 #include <unistd.h>
 #include <stdbool.h>
 
@@ -9,65 +11,67 @@
 #define THINKING 1
 #define HUNGRY 2
 
-int state[NUM_PHILOSOPHERS];
-sem_t mutex;
-sem_t chopsticks[NUM_PHILOSOPHERS];
-int total_eat_count = 0; // Total number of times all philosophers have eaten
+int semid;
+int total_eat_count = 0;
 
 void grab_forks(int philosopher_id) {
-    sem_wait(&mutex);
-    state[philosopher_id] = HUNGRY;
-    printf("Philosopher %d is hungry.\n", philosopher_id);
-    test(philosopher_id);
-    sem_post(&mutex);
-    sem_wait(&chopsticks[philosopher_id]);
+    struct sembuf sop[2];
+
+    sop[0].sem_num = philosopher_id;
+    sop[0].sem_op = -1;
+    sop[0].sem_flg = 0;
+
+    sop[1].sem_num = (philosopher_id + 1) % NUM_PHILOSOPHERS;
+    sop[1].sem_op = -1;
+    sop[1].sem_flg = 0;
+
+    semop(semid, sop, 2);
 }
 
 void put_away_forks(int philosopher_id) {
-    sem_wait(&mutex);
-    state[philosopher_id] = THINKING;
-    printf("Philosopher %d is thinking.\n", philosopher_id);
-    test((philosopher_id + NUM_PHILOSOPHERS - 1) % NUM_PHILOSOPHERS);
-    test((philosopher_id + 1) % NUM_PHILOSOPHERS);
-    sem_post(&mutex);
-}
+    struct sembuf sop[2];
 
-void test(int philosopher_id) {
-    if (state[philosopher_id] == HUNGRY &&
-        state[(philosopher_id + NUM_PHILOSOPHERS - 1) % NUM_PHILOSOPHERS] != EATING &&
-        state[(philosopher_id + 1) % NUM_PHILOSOPHERS] != EATING) {
-        state[philosopher_id] = EATING;
-        printf("Philosopher %d is eating.\n", philosopher_id);
-        sem_post(&chopsticks[philosopher_id]);
-        total_eat_count++;
-    }
+    sop[0].sem_num = philosopher_id;
+    sop[0].sem_op = 1;
+    sop[0].sem_flg = 0;
+
+    sop[1].sem_num = (philosopher_id + 1) % NUM_PHILOSOPHERS;
+    sop[1].sem_op = 1;
+    sop[1].sem_flg = 0;
+
+    semop(semid, sop, 2);
 }
 
 void* philosopher(void* arg) {
     int philosopher_id = *((int*)arg);
     while (total_eat_count < NUM_PHILOSOPHERS) {
         // Thinking
+        printf("Philosopher %d is thinking.\n", philosopher_id);
         sleep(1);
 
         // Hungry and trying to eat
         grab_forks(philosopher_id);
+        printf("Philosopher %d is hungry and grabbing forks.\n", philosopher_id);
 
         // Eating
+        printf("Philosopher %d is eating.\n", philosopher_id);
         sleep(1);
 
         // Done eating, put away forks
         put_away_forks(philosopher_id);
+        printf("Philosopher %d is done eating and putting away forks.\n", philosopher_id);
     }
 }
 
 int main() {
-    pthread_t philosophers[NUM_PHILOSOPHERS];
     int philosopher_ids[NUM_PHILOSOPHERS];
+    pthread_t philosophers[NUM_PHILOSOPHERS];
 
-    sem_init(&mutex, 0, 1);
+    // Create a semaphore set with one semaphore per philosopher
+    semid = semget(IPC_PRIVATE, NUM_PHILOSOPHERS, IPC_CREAT | 0666);
 
     for (int i = 0; i < NUM_PHILOSOPHERS; i++) {
-        sem_init(&chopsticks[i], 0, 0);
+        semctl(semid, i, SETVAL, 1); // Initialize semaphores to 1 (available)
         philosopher_ids[i] = i;
         pthread_create(&philosophers[i], NULL, philosopher, &philosopher_ids[i]);
     }
@@ -75,6 +79,9 @@ int main() {
     for (int i = 0; i < NUM_PHILOSOPHERS; i++) {
         pthread_join(philosophers[i], NULL);
     }
+
+    // Cleanup the semaphore set
+    semctl(semid, 0, IPC_RMID);
 
     return 0;
 }
